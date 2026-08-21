d4e52b57fe91426e64ab89f021dd2959dc99c881
project-genesis-ai <genesisai.research@gmail.com>
2026-08-21 20:51:08 +0530

    feat: expose universal biology API

---
+++ src/genesis/biology/__init__.py
@@ -0,0 +1,27 @@
+"""Universal biology primitives."""
+
+from .universal import (
+    BiologicalIndividual,
+    BiologicalTraits,
+    EcologicalInteraction,
+    EnvironmentExposure,
+    Genome,
+    IndividualIdentity,
+    InternalState,
+    Population,
+    SpeciesDefinition,
+    ecological_pressure,
+)
+
+__all__ = [
+    "BiologicalIndividual",
+    "BiologicalTraits",
+    "EcologicalInteraction",
+    "EnvironmentExposure",
+    "Genome",
+    "IndividualIdentity",
+    "InternalState",
+    "Population",
+    "SpeciesDefinition",
+    "ecological_pressure",
+]
