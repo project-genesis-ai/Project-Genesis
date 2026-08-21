b2f55b9ea4705dddaeb6d2054cf17a521de0433d
project-genesis-ai <genesisai.research@gmail.com>
2026-08-21 11:09:09 +0530

    feat: expose innovation civilization APIs

--- src/genesis/civilization/__init__.py
+++ src/genesis/civilization/__init__.py
@@ -1,5 +1,6 @@
-from .institutions import Institution
 from .government import Government
+from .innovation import Innovation, InnovationSystem
+from .institutions import Institution
 from .technology import Technology
 
-__all__ = ["Institution", "Government", "Technology"]
+__all__ = ["Institution", "Government", "Technology", "Innovation", "InnovationSystem"]
