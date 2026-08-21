4974c2cc1ec7491b9daee745f1243f7b49800bc1
project-genesis-ai <genesisai.research@gmail.com>
2026-08-21 10:35:07 +0530

    feat: expose ecological subsystems

--- src/genesis/life/__init__.py
+++ src/genesis/life/__init__.py
@@ -1,8 +1,23 @@
 """Living systems and ecological models."""
 
+from .behavior import EcologicalBehavior
 from .ecosystem import Ecosystem
+from .food_web import FoodWeb
+from .habitat import HabitatCell, HabitatMap
 from .organism import Organism
+from .population import PopulationDynamics
 from .species import Species, TrophicLevel
 from .systems import LifeSystem
 
-__all__ = ["Ecosystem", "LifeSystem", "Organism", "Species", "TrophicLevel"]
+__all__ = [
+    "Ecosystem",
+    "EcologicalBehavior",
+    "FoodWeb",
+    "HabitatCell",
+    "HabitatMap",
+    "LifeSystem",
+    "Organism",
+    "PopulationDynamics",
+    "Species",
+    "TrophicLevel",
+]
