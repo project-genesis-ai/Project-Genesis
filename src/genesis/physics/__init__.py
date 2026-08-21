aa8f4499316e271de9e0295932776f79d542d777
project-genesis-ai <genesisai.research@gmail.com>
2026-08-21 06:21:51 +0530

    feat: expose physics world API

--- src/genesis/physics/__init__.py
+++ src/genesis/physics/__init__.py
@@ -1,6 +1,8 @@
 """Deterministic physical models used by Genesis."""
 
+from .body import Body
 from .gravity import G, gravitational_acceleration
 from .vectors import Vec3
+from .world import PhysicsWorld
 
-__all__ = ["G", "Vec3", "gravitational_acceleration"]
+__all__ = ["Body", "G", "PhysicsWorld", "Vec3", "gravitational_acceleration"]
